1f543aec046f432e09c1fa04866671b2efe68313
SomnusVeritas <sites@skup.in>
2023-11-08 11:12:06 +0100

    simple content for create todo page

--- lib/pages/create_todo_page.dart
+++ lib/pages/create_todo_page.dart
@@ -9,8 +9,46 @@ class CreateTodoPage extends StatefulWidget {
 }
 
 class _CreateTodoPageState extends State<CreateTodoPage> {
+  String _title = '';
+  String _description = '';
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      floatingActionButton: _title.isNotEmpty
+          ? FloatingActionButton(
+              onPressed: () {},
+              child: const Icon(Icons.save),
+            )
+          : null,
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(label: Text('Title')),
+              textInputAction: TextInputAction.next,
+              onTapOutside: (event) => FocusScope.of(context).unfocus(),
+              onChanged: (value) {
+                if (value.isEmpty || (value.isNotEmpty && _title.isEmpty)) {
+                  setState(() {
+                    _title = value;
+                  });
+                } else {
+                  _title = value;
+                }
+              },
+            ),
+            TextField(
+              minLines: 3,
+              maxLines: 5,
+              decoration: const InputDecoration(label: Text('Description')),
+              onChanged: (value) => _description = value,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
